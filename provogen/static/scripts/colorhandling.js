73506e09b9d17e51d84d376ec7f23c5272f793e9
AlDacMac <alasdairmacgdev@gmail.com>
2022-01-04 21:24:51 +0000

    implement the colorhandling script and alter teh provogen template to use it 2

---
+++ provogen/static/scripts/colorhandling.js
@@ -0,0 +1,24 @@
+document.querySelector('#flagpicker').addEventListener(
+    "submit",
+    function (event){
+        event.preventDefault();
+        var placename = document.querySelector('#placename').value;
+        var box = document.querySelector('#colorbox');
+        while (box.hasChildNodes()){
+            box.removeChild(box.lastChild);
+        }
+        fetch($URL + '/flag-color?' + new URLSearchParams({
+            placename: placename
+        }))
+        .then(response => response.json())
+        .then(function (json){
+            gradstring = "linear-gradient(to right"
+            for(var i = 0; i < json.length; i++){
+                var rgb = json[i]['rgb'];
+                rgbstring = "rgb(" + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'
+                gradstring += ", " + rgbstring
+            }
+            box.style['background-image'] = gradstring
+        })
+    }
+)
