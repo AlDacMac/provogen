79dd2c7991c2d73f34150d60c999e7d16691e84f
AlDacMac <alasdairmacgdev@gmail.com>
2022-01-04 21:13:17 +0000

    implement the flag-color route

--- provogen/__init__.py
+++ provogen/__init__.py
@@ -1,5 +1,13 @@
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, jsonify)
+from flask.globals import request
 import os
+import requests
+from flaggart import wiki_flags, analysis, filehandling
+from io import BytesIO
+
+DEFAULT_USER_AGENT = 'ProvoGen/development'
+
+requests.utils.default_user_agent = lambda: DEFAULT_USER_AGENT
 
 def create_app(test_config=None):
     # create and configure the app
@@ -25,5 +33,20 @@ def create_app(test_config=None):
     @app.route('/')
     def provogen():
         return render_template('provogen.j2')
+
+    @app.route('/flag-color', methods=['GET'])
+    def flagColour():
+        print('test')
+        placename = request.args.get('placename')
+        print(placename)
+        wiki_search = wiki_flags.getflagpage(placename)
+        pagename = wiki_flags.selectflagpage(placename, wiki_search)
+        flagurl = wiki_flags.getflagurl(pagename)
+        print(f'url = {flagurl}')
+        flagrequest = requests.get(flagurl)
+        pil_image = filehandling.pngify(BytesIO(flagrequest.content))
+        colors = analysis.getdominantcolors(pil_image)
+        print(f"colors = {colors}")
+        return jsonify(colors)
 
     return app
