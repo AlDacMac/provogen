1163505b5f49b531ce3680d69ce145b5232127e0
AlDacMac <alasdairmacgdev@gmail.com>
2022-08-29 16:50:38 +0100

    get it to a point where the text resizes good

--- provogen/static/scripts/textresize.js
+++ provogen/static/scripts/textresize.js
@@ -2,7 +2,7 @@ function changefontsize() {
     var myInput = document.getElementById('placename');
     if(isOverflown(myInput)) {
         while(isOverflown(myInput)){
-            console.log('smallening')
+            console.log("padding = " + myInput.offsetHeight)
             fontsize -= (fontsize / 20);
             myInput.style.fontSize = fontsize + 'em';
         }
@@ -10,13 +10,18 @@ function changefontsize() {
         console.log('setting')
         fontsize = 1;
         myInput.style.fontSize = fontsize + 'em';
+
         while (isOverflown(myInput)){
+            console.log("padding = " + myInput.offsetHeight)
             fontsize -= (fontsize / 20);
             myInput.style.fontSize = fontsize + 'em';
+
         }
     }
   }
   
   function isOverflown(element) {
+    console.log(element.scrollWidth);
+    console.log(element.clientWidth);
       return element.scrollWidth > element.clientWidth;
   }
