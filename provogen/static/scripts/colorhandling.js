1714eb10363485e8ee79c671d4c1c69703209ebb
AlDacMac <alasdairmacgdev@gmail.com>
2022-08-31 14:43:40 +0100

    implement initial color box

--- provogen/static/scripts/colorhandling.js
+++ provogen/static/scripts/colorhandling.js
@@ -3,22 +3,26 @@ document.querySelector('#flagpicker').addEventListener(
     function (event){
         event.preventDefault();
         var placename = document.querySelector('#placename').value;
-        var box = document.querySelector('#colorbox');
-        while (box.hasChildNodes()){
-            box.removeChild(box.lastChild);
-        }
-        fetch($URL + '/flag-color?' + new URLSearchParams({
-            placename: placename
-        }))
-        .then(response => response.json())
-        .then(function (json){
-            gradstring = "linear-gradient(to right"
-            for(var i = 0; i < json.length; i++){
-                var rgb = json[i]['rgb'];
-                rgbstring = "rgb(" + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'
-                gradstring += ", " + rgbstring
-            }
-            box.style['background-image'] = gradstring
-        })
+        setColor(placename);
     }
 )
+
+function setColor(placename) {
+    var box = document.querySelector('#colorbox');
+    while (box.hasChildNodes()){
+        box.removeChild(box.lastChild);
+    }
+    fetch($URL + '/flag-color?' + new URLSearchParams({
+        placename: placename
+    }))
+    .then(response => response.json())
+    .then(function (json){
+        gradstring = "linear-gradient(to right"
+        for(var i = 0; i < json.length; i++){
+            var rgb = json[i]['rgb'];
+            rgbstring = "rgb(" + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'
+            gradstring += ", " + rgbstring
+        }
+        box.style['background-image'] = gradstring
+    })
+}
